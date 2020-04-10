8a0dcb1bae0aae9ebfbe0fa4802ac162cf978e1f
Andrey Mishchenko <ant1kguild@gmail.com>
2020-04-10 06:26:26 +0300

    feat: add Fibonacci impl

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -2,8 +2,14 @@ package subtask6
 
 class Fibonacci {
 
-    // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+        val list = mutableListOf(0, 1)
+        while (list[0] * list[1] < n) {
+            val temp = list[1]
+            list[1] = list[0] + list[1]
+            list[0] = temp
+        }
+        if (list[0] * list[1] > n) list.add(0) else list.add(1)
+        return list.toIntArray()
     }
 }
