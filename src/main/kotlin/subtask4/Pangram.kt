563a87fa2989759a93aa83b19531351d58c59b8a
Andrey Mishchenko <ant1kguild@gmail.com>
2020-04-10 05:46:57 +0300

    feat: add Pangram impl

--- src/main/kotlin/subtask4/Pangram.kt
+++ src/main/kotlin/subtask4/Pangram.kt
@@ -1,9 +1,65 @@
 package subtask4
 
+import com.sun.org.apache.xpath.internal.operations.Bool
+
 class Pangram {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): String {
-        throw NotImplementedError("Not implemented")
+        val isPangram = checkPangram(inputString)
+        return inputString
+            .split(" ")
+            .filter { it.isNotEmpty() && it[0] != '\n' }
+            .sortedBy { number(it, isPangram) }
+            .joinToString(" ") { createString(it, isPangram) }
     }
+
+
+    private fun checkPangram(inputString: String): Boolean {
+        val alphabet = mutableSetOf<Char>()
+        inputString.forEachIndexed { index, letter ->
+            if (inputString[index].isLetter()) alphabet.add(letter.toLowerCase())
+        }
+        return alphabet.size == 26
+    }
+
+    private fun number(s: String, isPangram: Boolean): Int {
+        var counter = 0
+        val vol =
+            arrayOf('A', 'E', 'I', 'O', 'U', "Y", 'a', 'e', 'i', 'o', 'u', 'y')
+        for (i in s.indices) {
+            if (isPangram) {
+                if (vol.contains(s[i])) counter++
+            } else {
+                if (s[i].isLetter() && !vol.contains(s[i])) counter++
+            }
+        }
+        return counter
+    }
+
+    private fun createString(inputString: String, isPangram: Boolean): String {
+        val vol =
+            arrayOf('A', 'E', 'I', 'O', 'U', "Y", 'a', 'e', 'i', 'o', 'u', 'y')
+        val temp = StringBuilder()
+        var counter = 0
+        if (isPangram) {
+            inputString.forEach { letter ->
+                if (vol.contains(letter)) {
+                    counter++
+                    temp.append(letter.toUpperCase())
+                } else temp.append(letter)
+            }
+        } else {
+            inputString.forEach { letter ->
+                if (letter.isLetter() && !vol.contains(letter)) {
+                    counter++
+                    temp.append(letter.toUpperCase())
+                } else temp.append(letter)
+            }
+        }
+        temp.insert(0, counter.toString())
+        return temp.toString()
+    }
+
+
 }
